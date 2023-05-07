be6a91c6b29238b9636d27f9c19537367d276d52
Noam Siegel <44804339+noamsgl@users.noreply.github.com>
2023-05-07 21:28:03 +0300

    style

--- stock_screener.py
+++ stock_screener.py
@@ -9,6 +9,7 @@ import configparser
 import os
 import sys
 import tkinter as tk
+from tkinter import ttk
 from datetime import datetime
 from io import StringIO
 from tkinter import Tk, filedialog, messagebox
@@ -129,7 +130,8 @@ def scrape() -> StringIO:
             "RANK1EXCEL": "https://www.zacks.com/portfolios/rank/rank_excel.php?rank=1&reference_id=all",
             "STOCKSCREENER": "https://www.zacks.com/screening/stock-screener",
             "MYSCREEN": "https://screener-api.zacks.com/myscreen.php?screen_type=1&_=1610807751953",
-            "ETFSCREENER": "https://www.zacks.com/screening/mutual-fund-screener"}
+            "ETFSCREENER": "https://www.zacks.com/screening/mutual-fund-screener",
+            "EXPORT.PHP": "https://screener-api.zacks.com/export.php"}
 
     print(20 * "*" + " Begin Scraping " + 20 * "*")
     with requests.Session() as s:
@@ -138,8 +140,62 @@ def scrape() -> StringIO:
         payload = {'username': config["APP"]["USER"],
                    'password': config["APP"]["PASSWORD"]}
         p = s.post(urls["HOME"], data=payload)
-        print("Downloading Rank1 Excel")
-        r = s.get(urls["RANK1EXCEL"])
+        print("Downloading CSV")
+        # r = s.get("https://screener-api.zacks.com/export.php", cookies={"TS01af1a9c": "0128dc3c314e3a8771cef59eb7c63d55b65fd2de7dc678efe984e20eed3128808e1ae2ed8922dd9a7bd6b89a09d88dbfc37ca1f411",
+        #                                                                 "visid_incap_2879622": "BwU+ZzsqS3Os+J8ublZzWEqpS2QAAAAAQUIPAAAAAAAFPRKwcjDkXbZt70pngjF0",
+        #                                                                 "_vwo_uuid_v2": "DD95DFD4653D61E27B7B69C86F1EA955A|1c60d0ee5cf96e0ed67e10f819b9f69e",
+        #                                                                 "AMCVS_3064401053DB594D0A490D4C%40AdobeOrg": "1",
+        #                                                                 "_vis_opt_s": "1%7C",
+        #                                                                 "_vis_opt_test_cookie": "1",
+        #                                                                 "s_v17": "DEF", "s_cc": "true",
+        #                                                                 "_vwo_uuid": "DD95DFD4653D61E27B7B69C86F1EA955A",
+        #                                                                 "_vwo_ds": "3%241682680139%3A80.47738532%3A%3A",
+        #                                                                 "_fbp": "fb.1.1682680140754.1256588369",
+        #                                                                 "cookie": "7542dcdc-31ae-4937-a702-3b9efacc153a",
+        #                                                                 "__qca": "P0-2018446936-1682680143486",
+        #                                                                 "__gads": "ID=c05dafbe053f1108-22661f2eafdd0057:T=1682680140:S=ALNI_MYVpJ5mNveruYK7bsLTyOeLjrUzuw",
+        #                                                                 "_au_1d": "AU1D-0100-001682680144-HJQUJ5J7-39QL",
+        #                                                                 "_au_last_seen_pixels": "eyJhcG4iOjE2ODI2ODAxNDQsInR0ZCI6MTY4MjY4MDE0NCwicHViIjoxNjgyNjgwMTQ0LCJydWIiOjE2ODI2ODAxNDQsInRhcGFkIjoxNjgyNjgwMTQ0LCJhZHgiOjE2ODI2ODAxNDQsImdvbyI6MTY4MjY4MDE0NCwiYWRvIjoxNjgyNjgwMTQ0LCJzb24iOjE2ODI2ODAxNDQsImltcHIiOjE2ODI2ODAxNDR9",
+        #                                                                 "_ga": "GA1.1.1697935371.1682680140",
+        #                                                                 "PHPSESSID": "p1cuq3hfv5d5208f1nd195v51t",
+        #                                                                 "visid_incap_2879609": "LgecmF6GTlWap3NufrL8hnKpS2QAAAAAQUIPAAAAAADK0n+SFzmc5TrhsX8b+jgH",
+        #                                                                 "_tt_enable_cookie": "1",
+        #                                                                 "_ttp": "dzXG97RnlLUO9AS-d39hiz-Juq4",
+        #                                                                 "AMCV_3064401053DB594D0A490D4C%40AdobeOrg": "77933605%7CMCIDTS%7C19478%7CMCMID%7C58174398938772432401021054286524967623%7CMCAAMLH-1683477550%7C6%7CMCAAMB-1683477550%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1682879950s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C4.5.1",
+        #                                                                 "s_vnum": "1682888400307%26vn%3D3",
+        #                                                                 "s_invisit": "true",
+        #                                                                 "undefined_s": "First%20Visit",
+        #                                                                 "cf60519feb1344e434b8444b746a915b": "cdaeeeba9b4a4c5ebf042c0215a7bb0e",
+        #                                                                 "incap_ses_1456_2879609": "d2dlX+rJrQk11MWH0sA0FFiaTmQAAAAAcZH8M5LGOO6TTREmyDqBNA==",
+        #                                                                 "__gpi": "UID=00000bf1a572c226:T=1682680140:RT=1682872920:S=ALNI_MbVcZKY9RBWMS1KvayTOdUjWxWFiw",
+        #                                                                 "s_gapv": "Logged%20In",
+        #                                                                 "incap_ses_433_2879609": "8sj7LnhRdUZopgS0CFQCBlWcTmQAAAAAKybE03SzWNMk6zcIFxGN4g==",
+        #                                                                 "incap_ses_1456_2879622": "vU2CKzX6oDxLcMqH0sA0FAShTmQAAAAA5lV5GL1S6dVPq/LONUlMqw==",
+        #                                                                 "_vwo_sn": "194490%3A1",
+        #                                                                 "s_p42": "screening%3A%20stock-screener",
+        #                                                                 "s_sq": "%5B%5BB%5D%5D",
+        #                                                                 "cto_bundle": "nxgQ3F84VVpxZUFLNTRwYldPJTJCekJVTkVrbmpPdlBOR1hFT0xyZ3lJODYlMkY2VCUyQkpQWm1WY1IxUEhCdERkWUVKWk5FU3pUbmhOazhWVXRJSjcxNHFkVGRqam4lMkJuRFhhaFRIQmlUMDljQTBLNWlzWUxXN2RIMG45SWtuNjdOanhxSXdGazRNVDNKbCUyQjY4NkhIS0lJMGVQSDNIenNBJTNEJTNE",
+        #                                                                 "s_nr": "1682874634807-Repeat",
+        #                                                                 "CURRENT_POS": "my_screen",
+        #                                                                 "_ga_MXXMZ1PBF7": "GS1.1.1682872750.3.1.1682874640.0.0.0"})
+        r = s.get("https://screener-api.zacks.com/export.php", cookies={"TS01af1a9c": "0128dc3c314e3a8771cef59eb7c63d55b65fd2de7dc678efe984e20eed3128808e1ae2ed8922dd9a7bd6b89a09d88dbfc37ca1f411", "visid_incap_2879622": "BwU+ZzsqS3Os+J8ublZzWEqpS2QAAAAAQUIPAAAAAAAFPRKwcjDkXbZt70pngjF0", "_vwo_uuid_v2": "DD95DFD4653D61E27B7B69C86F1EA955A|1c60d0ee5cf96e0ed67e10f819b9f69e", "AMCVS_3064401053DB594D0A490D4C%40AdobeOrg": "1", "_vis_opt_s": "1%7C", "_vis_opt_test_cookie": "1", "s_v17": "DEF", "s_cc": "true", "_vwo_uuid": "DD95DFD4653D61E27B7B69C86F1EA955A", "_vwo_ds": "3%241682680139%3A80.47738532%3A%3A", "_fbp": "fb.1.1682680140754.1256588369", "cookie": "7542dcdc-31ae-4937-a702-3b9efacc153a", "__qca": "P0-2018446936-1682680143486", "__gads": "ID=c05dafbe053f1108-22661f2eafdd0057:T=1682680140:S=ALNI_MYVpJ5mNveruYK7bsLTyOeLjrUzuw", "_au_1d": "AU1D-0100-001682680144-HJQUJ5J7-39QL", "_au_last_seen_pixels": "eyJhcG4iOjE2ODI2ODAxNDQsInR0ZCI6MTY4MjY4MDE0NCwicHViIjoxNjgyNjgwMTQ0LCJydWIiOjE2ODI2ODAxNDQsInRhcGFkIjoxNjgyNjgwMTQ0LCJhZHgiOjE2ODI2ODAxNDQsImdvbyI6MTY4MjY4MDE0NCwiYWRvIjoxNjgyNjgwMTQ0LCJzb24iOjE2ODI2ODAxNDQsImltcHIiOjE2ODI2ODAxNDR9", "_ga": "GA1.1.1697935371.1682680140", "PHPSESSID": "p1cuq3hfv5d5208f1nd195v51t", "visid_incap_2879609": "LgecmF6GTlWap3NufrL8hnKpS2QAAAAAQUIPAAAAAADK0n+SFzmc5TrhsX8b+jgH", "_tt_enable_cookie": "1", "_ttp": "dzXG97RnlLUO9AS-d39hiz-Juq4", "AMCV_3064401053DB594D0A490D4C%40AdobeOrg": "77933605%7CMCIDTS%7C19478%7CMCMID%7C58174398938772432401021054286524967623%7CMCAAMLH-1683477550%7C6%7CMCAAMB-1683477550%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1682879950s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C4.5.1",
+                                                                        "cf60519feb1344e434b8444b746a915b": "cdaeeeba9b4a4c5ebf042c0215a7bb0e",
+                                                                        "incap_ses_1456_2879609": "d2dlX+rJrQk11MWH0sA0FFiaTmQAAAAAcZH8M5LGOO6TTREmyDqBNA==",
+                                                                        "__gpi": "UID=00000bf1a572c226:T=1682680140:RT=1682872920:S=ALNI_MbVcZKY9RBWMS1KvayTOdUjWxWFiw",
+                                                                        "incap_ses_433_2879609": "8sj7LnhRdUZopgS0CFQCBlWcTmQAAAAAKybE03SzWNMk6zcIFxGN4g==",
+                                                                        "incap_ses_1456_2879622": "vU2CKzX6oDxLcMqH0sA0FAShTmQAAAAA5lV5GL1S6dVPq/LONUlMqw==",
+                                                                        "cto_bundle": "nxgQ3F84VVpxZUFLNTRwYldPJTJCekJVTkVrbmpPdlBOR1hFT0xyZ3lJODYlMkY2VCUyQkpQWm1WY1IxUEhCdERkWUVKWk5FU3pUbmhOazhWVXRJSjcxNHFkVGRqam4lMkJuRFhhaFRIQmlUMDljQTBLNWlzWUxXN2RIMG45SWtuNjdOanhxSXdGazRNVDNKbCUyQjY4NkhIS0lJMGVQSDNIenNBJTNEJTNE",
+                                                                        "s_p42": "portfolios%3A%20my-stock-portfolio",
+                                                                        "s_vnum": "1682888400307%26vn%3D4",
+                                                                        "s_invisit": "true",
+                                                                        "undefined_s": "First%20Visit",
+                                                                        "_ga_MXXMZ1PBF7": "GS1.1.1682877320.4.1.1682877321.0.0.0",
+                                                                        "s_gapv": "Logged%20In",
+                                                                        "s_nr": "1682877322127-Repeat",
+                                                                        "s_sq": "zacksprod%3D%2526c.%2526a.%2526activitymap.%2526page%253Dportfolios%25253A%252520my-stock-portfolio%2526link%253DStock%252520Screener%2526region%253Dscreener-tab%2526pageIDType%253D1%2526.activitymap%2526.a%2526.c%2526pid%253Dportfolios%25253A%252520my-stock-portfolio%2526pidt%253D1%2526oid%253Dhttps%25253A%25252F%25252Fwww.zacks.com%25252Fportfolios%25252Fmy-stock-portfolio%25252F%25253Ficid%25253Dportfolios-portfolios-nav_tracking-zcom-ma%2526ot%253DA",
+                                                                        "CURRENT_POS": "my_screen"})
+
+        # r = s.get(urls["RANK1EXCEL"])
         bytes_data = r.content
         string_data = str(r.content, 'utf-8')
         data_io = StringIO(string_data)
@@ -273,10 +329,10 @@ def save(df):
 if __name__ == '__main__':
     print(pyfiglet.figlet_format("Hello!!!"))
     print("Please select a menu option.")
-    button1 = tk.Button(root, text='Download and Score', command=lambda: execute(download=True), bg='#42b6f5',
-                        fg='white')
+    button1 = ttk.Button(root, text='Download and Score',
+                         command=lambda: execute(download=True))
     button1.pack(side=tk.TOP)
-    button2 = tk.Button(root, text='Score Only', command=lambda: execute(
-        download=False), bg='#42b6f5', fg='white')
+    button2 = ttk.Button(root, text='Score Only',
+                         command=lambda: execute(download=False))
     button2.pack(side=tk.BOTTOM)
     root.mainloop()
